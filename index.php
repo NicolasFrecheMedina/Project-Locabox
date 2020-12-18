b8c2133f325cff5a48bed549776e68851e9422dc
Nicolas Freche Medina <nicolas.freche@gmail.com>
2020-12-18 10:17:49 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+    <title>Locabox</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+
+</head>
+<body>
+<!-- Topbar -->
+    <div class="topbar">
+        <div>
+            <img style="margin-top:10px;" src="img/logo-locabox2.png" alt="logo-locabox">
+        </div>
+        <div id="menu">
+            <a href="#actualites"><div class="bouton_topbar">Actualités</div></a>
+            <a href="#"><div class="bouton_topbar">Les boxs</div></a>
+            <a href="#"><div class="bouton_topbar">Simulateur</div></a>
+            <a href="#"><div class="bouton_topbar">Le parc</div></a>
+            <a href="#"><div class="bouton_topbar"> Contact </div></a>
+        </div>
+    </div>
+<!--End Topbar -->
+<!-- Accueil -->
+<h6>BIENVENUE SUR</h6>
+    <div id="container">
+        <div class="slide_one"><img src="img/logo-locabox.png" alt=""></div>
+        <div class="slide_two"></div>
+        <div class="slide_three"></div>
+    </div>
+<!-- Actualités -->
+    <div id="actualites">
+
+        <h2>Actualités</h2>
+        <div class="slider">
+            <div class="slider2">Coucou</div>
+        </div>   
+        <div class="footer_general">
+            <div>
+                <button class="bouton_general"> Toute les actualités </button>
+            </div>           
+        </div> 
+        <div id="footer_general">
+            <div>
+                <a href="#"><img id="logo_footer" src="img/logo-locabox2.png" alt=""></a>
+            </div>
+        </div> 
+    </div>
+    
+<!-- End Actualités -->
+
+
+<script src="js/jQuery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
+<script src="js/demo.js"></script>
+</body>
+</html>
